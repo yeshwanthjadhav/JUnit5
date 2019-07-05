5a14305d800d93fe38605c273a59d28e7534387a
Yeshwanth Jadhav <yeshwanth.jadhav@gmail.com>
2019-07-05 18:17:10 -0500

    @BeforeEach

--- src/main/java/junit5/MathUtils.java
+++ src/main/java/junit5/MathUtils.java
@@ -9,4 +9,8 @@ public class MathUtils {
 	public int add(int a, int b){
 		return a+b;
 	}
+	
+	public int divide(int a, int b){
+		return a/b;
+	}
 }
--- src/test/java/junit5/MathUtilsTest.java
+++ src/test/java/junit5/MathUtilsTest.java
@@ -1,35 +1,48 @@
 package junit5;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 import java.time.Duration;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-public class MathUtilsTest{
+public class MathUtilsTest {
 
- @Test
- void test(){
-	 MathUtils mu = new MathUtils();
-	 int expected = 2;
-	 int actual = mu.add(1, 1);
-	 assertEquals(expected, actual);
-	 // fail("Not done"); 
+	MathUtils mu;
+	
+	@BeforeEach
+	void init(){
+		mu = new MathUtils();		
+	}
+	
+	@Test
+	void testDivide() {
+		Assertions.assertThrows(ArithmeticException.class, () -> mu.divide(1, 0), "Divide ");
 
-	 
-	 Assertions.assertTimeout(Duration.ofMillis(100), 
-			 () -> { Thread.sleep(200); return "result";});
-	 
-	 //assertEquals(expected, actual);
-	 //assertArrayEquals(expecteds, actuals);
-	 //assertIterableEquals(expectedArray, actualArray);
-	 
-	 //https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
- }
- 
+	}
+
+	@Test
+	void test() {
+		int expected = 2;
+		int actual = mu.add(1, 1);
+		Assertions.assertEquals(expected, actual);
+		// fail("Not done");
+
+		Assertions.assertTimeout(Duration.ofMillis(100), () -> {
+			Thread.sleep(20);
+			return "result";
+		});
+
+		// assertEquals(expected, actual);
+		// assertArrayEquals(expecteds, actuals);
+		// assertIterableEquals(expectedArray, actualArray);
+
+		// https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
+	}
+
 }
